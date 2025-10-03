d4fab013ab0c6eee87443f48be18afaa68f7a847
Avik Sain <avik.sain@capsitech.com>
2025-10-03 15:57:40 +0530

    Refactor SideBar component structure and update MainLayout to use new item hierarchy

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,29 +1,27 @@
-import { Col, Layout, Row } from 'antd';
-import React from 'react'
-import Sider from 'antd/es/layout/Sider';
-import { Content } from 'antd/es/layout/layout';
-import TopHeader from '../components/TopHeader';
-import SideBar from '../components/SideBar';
+import { Col, Layout, Row } from "antd";
+import React from "react";
+import Sider from "antd/es/layout/Sider";
+import { Content } from "antd/es/layout/layout";
+import { SideBar, TopHeader } from "../components";
 
 const MainLayout: React.FC = () => {
   return (
-    <Row justify={'center'}>
-      <Col md={18} xs={24}>
+    <Row justify={"center"}>
+      <Col xl={18} xs={24}>
         <Layout>
           <TopHeader />
           <Layout>
-            <Sider className='bg-white ml-2 p-2 rounded-md ant-layout-main-content scrollable'>
+            <Sider className="bg-white ml-2 p-2 rounded-md ant-layout-main-content scrollable">
               <SideBar />
             </Sider>
-            <Content className='bg-white mx-2 p-2 rounded-md ant-layout-main-content scrollable'>
+            <Content className="bg-white mx-2 p-2 rounded-md ant-layout-main-content scrollable">
               <h1>Under Development</h1>
             </Content>
           </Layout>
         </Layout>
       </Col>
-
     </Row>
-  )
-}
+  );
+};
 
 export default MainLayout;
