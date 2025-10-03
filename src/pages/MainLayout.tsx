d9abf22766bfdac71656110f8021b14345aa0c91
Himanshu Sharma <himanshu.sharma@capsitech.com>
2025-10-03 18:47:28 +0530

    Remove unused components and example text from MainLayout

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -7,10 +7,6 @@ import SideBar from '../components/SideBar';
 import '../css/common.css'
 import { AlignLeftOutlined } from '@ant-design/icons';
 import MobileSidebar from '../components/MobileSidebar';
-import Heading from '../components/Heading';
-import Paragraph from '../components/Paragraph';
-import SubHeading from '../components/SubHeading';
-import Title from '../components/Title';
 
 const MainLayout: React.FC = () => {
 
@@ -33,10 +29,6 @@ const MainLayout: React.FC = () => {
             </Sider>
             <Content className='bg-white mx-2 p-2 rounded-md ant-layout-main-content scrollable docs-shadow'>
               <AlignLeftOutlined onClick={onMenuClick} className='text-md cursor-pointer side-bar-menu-collapse theme-color pr-4' style={{ lineHeight: "1px" }} />
-              <Heading text='Hello world!' />
-              <Paragraph text={<>Lorem Ipsum is <Title text='simply dummy text of the' /> printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</>} />
-              <SubHeading text='This is a subheading' />
-              <Paragraph text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." />
             </Content>
           </Layout>
         </Layout>
