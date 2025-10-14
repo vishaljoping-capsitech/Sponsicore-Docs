405c339188e2a27d9d3e58a3640f06edbd0d50ab
anoushkamishra-capsitech <anoushka.mishra@capsitech.com>
2025-10-14 19:44:24 +0530

    Refactor Overview component to use TypeScript types and improve formatting

--- src/pages/adminPanel/overview/overview.tsx
+++ src/pages/adminPanel/overview/overview.tsx
@@ -1,19 +1,23 @@
+import React from "react";
+import { AnchorItemProps } from "../../../types/Types";
 
-const Overview = () => {
+const Overview: React.FC & { getAnchorItems?: () => AnchorItemProps[] } = () => {
   return (
     <>
-    <div>Overview</div>
-    <ol>
-      <li id='part-1'>Part-1</li>
-      <li id='part-2'>Part-2</li>
-      <li id='part-3'>Part-3</li>
-    </ol>
+      <div>Overview</div>
+      <ol>
+        <li id="part-1">Part-1</li>
+        <li id="part-2">Part-2</li>
+        <li id="part-3">Part-3</li>
+      </ol>
     </>
-  )
-}
+  );
+};
+
 Overview.getAnchorItems = () => [
-  { key: 'part-1', href: '#part-1', title: 'Part 1' },
-  { key: 'part-2', href: '#part-2', title: 'Part 2', },
-  { key: 'part-3', href: '#part-3', title: 'Part 3', },
-]
-export default Overview
+  { key: "part-1", href: "#part-1", title: "Part 1" },
+  { key: "part-2", href: "#part-2", title: "Part 2" },
+  { key: "part-3", href: "#part-3", title: "Part 3" },
+];
+
+export default Overview;
